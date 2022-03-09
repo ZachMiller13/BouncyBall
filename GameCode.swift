7011826f63dc3e5299129f813a39723f0370f125
Zachary Miller <zach_miller13@icloud.com>
2022-03-08 20:35:35 -0500

    Added barrier to game

--- GameCode.swift
+++ GameCode.swift
@@ -1,5 +1,18 @@
 import Foundation
+// initializes the circle
 let circle = OvalShape(width: 150, height: 150)
+// initializes the barriers
+let barrierWidth = 300.0
+let barrierHeight = 25.0
+// assigns locations to barriers
+let barrierPoints = [
+Point(x: 0, y: 0),
+Point(x: 0, y: barrierHeight),
+Point(x: barrierWidth, y: barrierHeight),
+Point(x: barrierWidth, y: 0)
+]
+
+let barrier = PolygonShape(points: barrierPoints)
 /*
 The setup() function is called once when the app launches. Without it, your app won't compile.
 Use it to set up and start your app.
@@ -11,6 +24,17 @@ of a function.
 */
 
 func setup() {
+// Adds circle to the Window
     circle.position = Point(x: 250, y: 400)
     scene.add(circle)
+    //Adds acceleration downwards, caused by gravity
+    circle.hasPhysics = true
+    
+    
+    //Add a barrier to the scene
+    barrier.position = Point(x: 200, y: 150)
+    barrier.hasPhysics = true
+    scene.add(barrier)
+    
+    barrier.isImmobile = true
 }
